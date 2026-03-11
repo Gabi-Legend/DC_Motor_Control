21b29a10ab320119a2fd3d9a6e585de62664cf90
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-03-11 10:57:36 +0200

    setup for motor, also checking if the l298n works

--- motor_control.ino
+++ motor_control.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+int enA = 5;
+int in1 = 6;
+int in2 = 7;
 
+void setup() {
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+
+  digitalWrite(in1, HIGH); 
+  digitalWrite(in2, LOW);
+  digitalWrite(enA, HIGH); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
